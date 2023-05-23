8b0c02f88f12ea0f83f95e51117190f2bce29e11
Rafael Angelim <125464062+RafaelAngelim@users.noreply.github.com>
2023-05-23 17:35:49 -0300

    Add files via upload

--- drink.c
+++ drink.c
@@ -2,11 +2,20 @@
 
 int main(){
     
-    int fri, bottle, lbo, limes, slices, salt, ml, mp, numbsl;
+    int fri, bottle, lbo, limes, slices, salt, ml, mp, numbsl, sau, quantity, drinks;
     scanf("%d %d %d %d %d %d %d %d", &fri, &bottle, &lbo, &limes, &slices, &salt, &ml, &mp);
 
+    
     numbsl = limes * slices;
-    
-    
+    quantity = bottle * lbo / ml;
+    sau = salt / mp;
 
-}
+    if(quantity <= numbsl && quantity <= sau ){drinks = quantity / fri;  }
+    else if(numbsl <= quantity && numbsl <= sau){drinks = numbsl / fri;}
+    else if(sau <= quantity && sau <= numbsl){drinks = sau / fri;}
+
+    printf("%d\n", drinks);
+
+    return 0;
+
+}    
