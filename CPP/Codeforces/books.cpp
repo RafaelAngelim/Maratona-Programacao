4be10a9947b3597b1a86ac92ed8cdf23f3ba0d7e
RafaelAngelim <rafael.angelim@hotmail.com>
2023-11-14 18:17:26 -0300

    Solved

--- CPP/Codeforces/books.cpp
+++ CPP/Codeforces/books.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
     int N, T;
     vector<int>books;
     int wen, st = 0, sum = 0, qty = 0;
+    cin >> N >> T;
     for(int c = 0; c < N; c++){
         cin >> wen;
         books.push_back(wen);
     }
     for(int i = 0; i < N; i++){
         while(st < N && books[st] + sum <= T){
-            sum += books[st]; st++;
+            sum += books[st];
+            st++;
         }
         qty = max(qty, st-1); sum -= books[i];
     }
     cout << qty << endl;
+    
+    
 
     return 0;
 }
